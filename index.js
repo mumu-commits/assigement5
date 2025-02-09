1c59e9b21b61e662b8b204646f1f0b58d6ce48ca
mumu-commits <mujeebkaka78@gmail.com>
2025-02-09 19:47:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var username = "mujeeb"
+console.log("user name " +  username) 
+myName = "mujeeb ur rahman"
+console.log("myName is "  +  myName)
+var hello = ("hello world")
+console.log()
+alert("student bio data")
+alert("17 year old ")
+alert("certified mobile application development")
+alert("plzza plzz plz pl p")
+alert("mujeebkaka78@gmail.com")
+alert("i am trying to learn from the book a smarter way to leart Javascript")
+document.write("yah!I can write HTML content through JavaScript")
+alert("______________&%^___________________")
+<"br">
+document.write("--END--")
